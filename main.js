df3c87efabce48a9b5d0942af61f1ab21e0b6526
VladNedr <nedr2517@mail.ru>
2023-01-26 20:13:11 +0300

    add: variables

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+const title = 123;
+const screens = document;
+const screenPrice = 345;
+let roolback = addEventListener;
+const fullPrice = 789;
+let adaptive = document.querySelector;
 
+alert("Hello My Friends");
+console.log("Hello My Friends");
