4f1c22edfe06be18a2ca61a34209d21d91298518
VladNedr <nedr2517@mail.ru>
2023-01-28 17:19:25 +0300

    completed 2 homework

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
-const title = 123;
-const screens = 345;
-const screenPrice = 345;
-let roolback = 345;
-const fullPrice = 789;
-let adaptive = 123;
+const title = "Java Script";
+console.log(typeof title);
 
-alert("Hello My Friends");
-console.log("Hello My Friends");
+const screens = "Простые, Сложные, Интерактивные";
+console.log(screens.length);
+console.log(screens.toLowerCase().split());
+
+const screenPrice = 25;
+console.log(" Стоимость верстки экранов 25 рублей/ долларов/гривен/юани ");
+
+let roolback = 10;
+
+const fullPrice = 40000;
+console.log(typeof fullPrice);
+console.log("Стоимость разработки сайта 40000 рублей/ долларов/гривен/юани");
+
+let adaptive = true;
+console.log(typeof adaptive);
+
+console.log(40000 * (10 / 100));
